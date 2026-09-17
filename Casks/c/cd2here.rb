069304f8d0163180ba8ba7502303afbee13874c8
ActivationEnergy <244162811+ActivationEnergy@users.noreply.github.com>
2026-09-17 12:11:30 +0800

    cd2here 0.3.2
    Bump to v0.3.2:
    - version: 0.3.1 → 0.3.2
    - sha256: 7c4673... (v0.3.1) → 091fa4c8... (v0.3.2)
    
    Brew style: no offenses detected.

--- Casks/c/cd2here.rb
+++ Casks/c/cd2here.rb
@@ -1,6 +1,6 @@
 cask "cd2here" do
-  version "0.3.1"
-  sha256 "7c4673765f9252b9a6eee18ace86f9806a587dcec76987acffd948bbf6287aab"
+  version "0.3.2"
+  sha256 "091fa4c8998023c7eec7530b1357b4a153f99353a7e9ab7c92164934b83575f6"
 
   url "https://github.com/ActivationEnergy/cd2here/releases/download/v#{version}/cd2here-v#{version}-darwin-universal.zip"
   name "cd2here"
